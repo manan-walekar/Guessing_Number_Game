516226a6ce2d6942fd57b5e5a79c2c92dffc2304
Manan <manan.walekar0404@gmail.com>
2024-08-11 00:55:32 +0530

    commited

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int randomnumber;
+        int number;
+
+        Scanner scan = new Scanner(System.in);
+
+        Random random = new Random();
+        randomnumber = random.nextInt(100) - 1;
+
+        // System.out.println("RANDOM NO. IS = " + randomnumber);
+        int trycount = 0;
+        while (true) {
+            System.out.println("Guess the number from (1-100) : ");
+            number = scan.nextInt();
+            // System.out.println(number);
+            trycount++;
+
+            if (randomnumber == number) {
+                System.out.println("HURRAY ! YOU WIN");
+                System.out.println("IT TOOK YOU " + trycount + " TRIES :)");
+                break;
+            }
+
+            else if (randomnumber > number) {
+                System.out.println("NOPE THE NUMBER IS GREATER. TRY AGAIN.");
+                System.out.println("");
+            }
+            else if(number>100){
+                System.out.println("Number is Invalid !");
+                System.out.println("Restart Again");
+                break;
+            }
+
+            else {
+                System.out.println("NOPE THE NUMBER IS SMALLER. TRY AGAIN.");
+                System.out.println("");
+            }
+        }
+
     }
 }
